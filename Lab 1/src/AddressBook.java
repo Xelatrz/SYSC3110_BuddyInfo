d2ccf6f06d24580a2a9c1a851e8d24c4633debef
Taylor Brumwell <taylor.brumwell@gmail.com>
2025-09-17 19:15:35 -0400

    I add some function calls to AddressBook.java to test Git Commit

--- Lab 1/src/AddressBook.java
+++ Lab 1/src/AddressBook.java
@@ -17,6 +17,6 @@ public class AddressBook {
     }
 
     public static void main(String[] args) {
-        System.out.println("Address Book");
+        System.out.println("Address Books");
     }
 }
