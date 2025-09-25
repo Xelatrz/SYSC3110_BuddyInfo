f99728ba0d5404cc3f602668a459d685323152e8
Taylor Brumwell <taylor.brumwell@gmail.com>
2025-09-25 08:30:49 -0400

    push buddyInfo and AddressBook file to github

---
+++ BuddyInfo Project/src/BuddyInfo.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BuddyInfo {
+
+    private String name;
+    private String address;
+    private String phone_number;
+
+    public BuddyInfo(String name, String address, String phone_number) {
+        this.name = name;
+        this.address = address;
+        this.phone_number = phone_number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static void main(String[] args) {
+        BuddyInfo buddy = new BuddyInfo("Bobby", "100 Rideau Crescent", "123-456-7890");
+        System.out.println("Hello" + buddy.getName());
+    }
+}
+
